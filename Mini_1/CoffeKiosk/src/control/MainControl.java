899cd20ece84e44bcfacef988e3b6cf18173ec04
muse9312 <muse9312@naver.com>
2021-12-01 17:46:31 +0900

    MainControl  modify

--- Mini_1/CoffeKiosk/src/control/MainControl.java
+++ Mini_1/CoffeKiosk/src/control/MainControl.java
@@ -1,7 +1,5 @@
 package control;
 
-import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 
 import Banner.BannerPage;
@@ -11,18 +9,8 @@ public class MainControl extends JFrame {
 
     public static void main(String[] args) {
 
-        EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                try {
-                    new BannerPage();
-                    new BannerSlide();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-
-            }
-
-        });
+        new BannerSlide();
+        new BannerPage();
 
     }
 }
